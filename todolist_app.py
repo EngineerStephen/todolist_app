d426b07806085cdc174f841f16daee28fb1f0be4
Love Divine <stephen.asiedu17@gmail.com>
2024-06-17 23:49:49 -0400

    fixed input: entering the actual task instead of the numbers now

--- todolist_app.py
+++ todolist_app.py
@@ -1,5 +1,5 @@
 print()
-user_name = input("Hello, I'm Sandra, your virtual assistant. I will help you organize your daily tasks. Please tell me your name:   ").capitalize()
+user_name = input("Hello, I'm Sandra, your virtual assistant. I will help you organize your daily tasks. Please tell me your name: ").capitalize()
 print()
 print(user_name + ", Nice to meet you!", end=" ")
 
@@ -15,31 +15,43 @@ for i, item in menu_enum:
     print(i, item)
     
 def user_choices(menu):
+    tasks = []
     print()
     while True:
-        user_selection = int(input("Please enter the number for the task you would like to do today: "))
-        if user_selection in range(1, len(menu) + 1):
+        user_selection = input("Please enter the task you would like to do today: ")
+        #tried: str(int(input("Please enter the number for the task you would like to do today: ")))
+        #tried: int(input("Please enter the number for the task you would like to do today: "))
+        if user_selection in menu:
+        #tried: if user_selection >= 0 and user_selection <= len(menu) + 1:
             print()
-            print("You have chosen to", menu[user_selection], "today, I will remind you to do this task. ")
+            print("You have chosen to", user_selection, "today, I will remind you to", tasks.append(user_selection), "later.")
+        else:
+            print("Invalid input. Please try again.")
+            continue
         user_add = input("Would you like to add another task? (yes/no)")
         if user_add == "yes":
             print()
-            user_more = int(input("Please enter the number for the task you would like to add: "))
-            if user_more in range(1, len(menu) + 1):
-                menu[user_selection] = menu[user_selection] + " and " + menu[user_more]
-                print("You have chosen to add", menu[user_selection], " to your activities today, I will remind you. ") 
-        elif user_add == "no":
-            print("Ok. I will remind you to", menu[user_selection], ".")
-            break
-        else:
-             if user_more not in range(1, len(menu) + 1):
+            user_more = input("Please enter the task you would like to add: ")
+            #tried: str(int(input("Please enter the number for the task you would like to do today: ")))
+            #tried: int(input("Please enter the number for the task you would like to do today: "))
+            # if user_more in range(1, len(menu) + 1):
+            if user_more in menu:
+                tasks.append(user_more)
+                print()
+                print("You have chosen to add", tasks.append(user_more), " to your activities today, I will remind you. ")
+            else:
                 print("Invalid input. Please try again.")
-        
-        
-     
-
-
-
+                continue
+        elif user_add == "no":
+                print()
+                print("Ok. I will remind you to: ")
+                for task in tasks:
+                    print("-" + task)
+                    print()
+                print("Have a great day!")
+                break
+        else:
+            print("Invalid input. Please try again.")
 
 
 user_choices(menu)
